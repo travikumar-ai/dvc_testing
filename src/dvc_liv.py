3b53240249ac040a52ad6a682205a9c505494ba9
Ravi Kumar <thotakuriravi@gmail.com>
2025-09-22 17:40:50 +0530

    exp done

---
+++ src/dvc_liv.py
@@ -0,0 +1,19 @@
+import random
+import sys
+
+from dvclive import Live
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Arguments error. Usage:\n")
+    sys.stderr.write(f"\tpython {sys.argv[0]} <epochs>\n")
+    sys.exit(1)
+
+with Live(save_dvc_exp=True) as live:
+    epochs = int(sys.argv[1])
+    live.log_param("epochs", epochs)
+    for epoch in range(epochs):
+        live.log_metric("train/accuracy", epoch + random.random())
+        live.log_metric("train/loss", epochs - epoch - random.random())
+        live.log_metric("val/accuracy", epoch + random.random())
+        live.log_metric("val/loss", epochs - epoch - random.random())
+        live.next_step()
